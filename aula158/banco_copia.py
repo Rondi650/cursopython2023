6f9f6eec8844b1b0e8a66cb53855d2f109fade5b
Rondi650 <rondinellenunes123@gmail.com>
2026-02-01 22:49:02 -0300

    retirado print do exemplo do professor da classe banco para melhor legibilidade.

--- aula158/banco_copia.py
+++ aula158/banco_copia.py
@@ -15,30 +15,25 @@ class Banco:
 
     def _checa_agencia(self, conta: contas.Conta):
         if conta.agencia in self.agencias:
-            print('_checa_agencia', True)
             return True
-        print('_checa_agencia', False)
         return False
 
     def _checa_cliente(self, cliente: pessoas.Cliente):
         if cliente in self.clientes:
-            print('_checa_cliente', True)
             return True
-        print('_checa_cliente', False)
         return False
 
     def _checa_conta(self, conta: contas.Conta):
         if conta in self.contas:
-            print('_checa_conta', True)
             return True
-        print('_checa_conta', False)
         return False
 
-    def _checa_se_conta_e_do_cliente(self, cliente: pessoas.Cliente, conta: contas.Conta):
+    def _checa_se_conta_e_do_cliente(
+            self,
+            cliente: pessoas.Cliente,
+            conta: contas.Conta):
         if conta is cliente.conta:
-            print('_checa_se_conta_e_do_cliente', True)
             return True
-        print('_checa_se_conta_e_do_cliente', False)
         return False
 
     def autenticar(self, cliente: pessoas.Cliente, conta: contas.Conta):
@@ -54,15 +49,15 @@ class Banco:
 
 
 if __name__ == '__main__':
-    
+
     c1 = pessoas.Cliente('Luiz', 30)
     cc1 = contas.ContaCorrente(111, 222, 0, 0)
     c1.conta = cc1
-    
+
     c2 = pessoas.Cliente('Maria', 18)
     cp1 = contas.ContaPoupanca(112, 223, 100)
     c2.conta = cp1
-    
+
     banco = Banco()
     banco.clientes.extend([c1, c2])
     banco.contas.extend([cc1, cp1])
