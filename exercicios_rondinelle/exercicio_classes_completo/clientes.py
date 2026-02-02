de75c2b015697583b40f77402d76216af73b3243
Rondi650 <rondinellenunes123@gmail.com>
2026-02-01 21:25:22 -0300

    inclusao de atributo limite na classe ContaCorrente para exercicio

--- exercicios_rondinelle/exercicio_classes_completo/clientes.py
+++ exercicios_rondinelle/exercicio_classes_completo/clientes.py
@@ -1,35 +1,43 @@
-from abc import ABC, abstractmethod
+from abc import ABC
 from contas import ContaCorrente, ContaPoupanca
 
-class Pessoa(ABC):
-    @abstractmethod
+
+class Pessoa():
     def __init__(self, nome: str, idade: int) -> None:
         self._nome = nome
         self._idade = idade
-        
+
     @property
     def nome(self) -> str:
         return self._nome
-    
+
+    @nome.setter
+    def nome(self, valor):
+        self._idade = valor
+
     @property
     def idade(self) -> int:
         return self._idade
-        
-    
+
+    @idade.setter
+    def idade(self, valor):
+        self._idade = valor
+
+
 class Cliente(Pessoa):
     # contas: list[ContaCorrente | ContaPoupanca] = []
-    
+
     def __init__(self,
-                 nome: str, 
-                 idade: int, 
+                 nome: str,
+                 idade: int,
                  conta: ContaCorrente | ContaPoupanca) -> None:
         super().__init__(nome, idade)
         self._conta = conta
         # Cliente.contas.append(self._conta)
-        
+
     @property
     def conta(self) -> ContaCorrente | ContaPoupanca:
         return self._conta
-        
+
     def __repr__(self) -> str:
         return f'Cliente: {self.nome} Idade:{self.idade} {self.conta}'
--- exercicios_rondinelle/exercicio_classes_completo/main.py
+++ exercicios_rondinelle/exercicio_classes_completo/main.py
@@ -5,21 +5,21 @@ from bancos import Banco
 
 os.system('cls')
 
-conta1 = ContaCorrente('123', '456')
+conta1 = ContaCorrente('123', '456', 500)
 conta1.depositar(500)
 conta1.depositar(300)
-conta1.sacar(850)
-conta1.sacar(50)
-conta1.sacar(1)
+conta1.sacar(1050)
 cliente1 = Cliente('rondi', 35, conta1)
+cliente1._nome = 'joao'
+cliente1._nome = 'maria'
 autentica = Banco(cliente1, conta1, conta1)
 autentica.autenticacao()
 print(cliente1)
 
-print()
+# print()
 
-conta2 = ContaPoupanca('789', '321')
-conta2.depositar(300)
-conta2.sacar(400)
-cliente1 = Cliente('samara', 29, conta2)
-print(cliente1)
+# conta2 = ContaPoupanca('789', '321')
+# conta2.depositar(300)
+# conta2.sacar(400)
+# cliente1 = Cliente('samara', 29, conta2)
+# print(cliente1)
