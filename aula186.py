c468062d9b31db7c47df4c5a0aec5b565a19f016
Rondi650 <rondinellenunes123@gmail.com>
2026-02-11 09:40:56 -0300

    aulas dia 11/02/2026 compactando e descompactando pastas.

--- aula186.py
+++ aula186.py
@@ -4,9 +4,11 @@ import shutil
 from pathlib import Path
 from zipfile import ZipFile
 
+os.system('cls')
+
 # Caminhos
 CAMINHO_RAIZ = Path(__file__).parent
-CAMINHO_ZIP_DIR = CAMINHO_RAIZ / 'aula_186_diretorio_zip'
+CAMINHO_ZIP_DIR = CAMINHO_RAIZ / 'aula186_diretorio_zip'
 CAMINHO_COMPACTADO = CAMINHO_RAIZ / 'aula186_compactado.zip'
 CAMINHO_DESCOMPACTADO = CAMINHO_RAIZ / 'aula186_descompactado'
 
@@ -20,22 +22,25 @@ shutil.rmtree(CAMINHO_DESCOMPACTADO, ignore_errors=True)
 # Cria o diretório para a aula
 CAMINHO_ZIP_DIR.mkdir(exist_ok=True)
 
-
 def criar_arquivos(qtd: int, zip_dir: Path):
-    for i in range(qtd):
-        texto = 'arquivo_%s' % i
+    for i in range(1, qtd+1):
+        texto = f'arquivo_{i}'
         with open(zip_dir / f'{texto}.txt', 'w') as arquivo:
             arquivo.write(texto)
 
 
-criar_arquivos(10, CAMINHO_ZIP_DIR)
+criar_arquivos(20, CAMINHO_ZIP_DIR)
 
 # Criando um zip e adicionando arquivos
 with ZipFile(CAMINHO_COMPACTADO, 'w') as zip:
     for root, dirs, files in os.walk(CAMINHO_ZIP_DIR):
         for file in files:
-            # print(file)
-            zip.write(os.path.join(root, file), file)
+            zip.write(filename=os.path.join(root, file), arcname=file)
+            
+with ZipFile(CAMINHO_COMPACTADO, 'w') as zip:           
+    for file in os.listdir(CAMINHO_ZIP_DIR):
+        zip.write(filename=os.path.join(os.path.abspath(CAMINHO_ZIP_DIR), file),
+                  arcname=file)
 
 # Lendo arquivos de um zip
 with ZipFile(CAMINHO_COMPACTADO, 'r') as zip:
