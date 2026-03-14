026d1897be29d7d0b5f88d019c4a0796a58261c5
Rondi650 <rondinellenunes123@gmail.com>
2026-03-14 13:02:40 -0300

    forma correta e errada de fazer o INSERT nas tabelas de dados, uso correto com placeholders (?,?) no lugar de VALUES, os valores sao passados posteriormente dentro de execute(sql, [parametro1,parametro2])

--- aula205_sql/main.py
+++ aula205_sql/main.py
@@ -10,6 +10,24 @@ TABLE_NAME = 'customers'
 connection = sqlite3.connect(DB_FILE)
 cursor = connection.cursor()
 
+################################################################################
+
+# Apaga todos os registros da tabela
+cursor.execute(
+    f'DELETE FROM {TABLE_NAME}'
+)
+connection.commit()
+
+################################################################################
+
+# Apaga o historicos de ids automaticos
+cursor.execute(
+    f'DELETE FROM sqlite_sequence WHERE name="{TABLE_NAME}"'
+)
+
+################################################################################
+
+# Cria a tabela
 cursor.execute(
     f'CREATE TABLE IF NOT EXISTS {TABLE_NAME}'
     '( id INTEGER PRIMARY KEY AUTOINCREMENT,' 
@@ -17,8 +35,36 @@ cursor.execute(
     'weight REAL'
     ')'
 )
-
 connection.commit()
+
+################################################################################
+
+# Registra valores nas colunas da tabela
+# Forma errada, devido a sql injection - para valores do usuario
+cursor.execute(
+    f'INSERT INTO {TABLE_NAME} (id, name, weight)'
+    'VALUES'
+    '(NULL, "Samara", 85),'
+    '(NULL, "Heitor", 35)'
+)
+connection.commit()
+
+################################################################################
+
+# Forma correta
+# Com placeholders os valores sao passados posteriormente
+sql = (f'INSERT INTO {TABLE_NAME} (name, weight) '
+    'VALUES '
+    '(?,?)')
+
+cursor.execute(sql, ['Pedro',85]) # execute passa apenas 1 valor
+connection.commit()
+
+################################################################################
+
+
+
+################################################################################
 
 cursor.close()
 connection.close()
