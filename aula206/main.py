eac387ccc1302991c1057386f210465c03fdab25
Rondi650 <rondinellenunes123@gmail.com>
2026-03-16 19:50:40 -0300

    iinclusaom de dados de teste na tabela do mysql "customers"

--- aula206/main.py
+++ aula206/main.py
@@ -2,6 +2,7 @@
 # Doc: https://pymysql.readthedocs.io/en/latest/
 # Pypy: https://pypi.org/project/pymysql/
 # GitHub: https://github.com/PyMySQL/PyMySQL
+from curses.ascii import TAB
 import os
 import dotenv
 import pymysql
@@ -10,6 +11,8 @@ TABLE_NAME = 'customers'
 
 os.system('clear')
 dotenv.load_dotenv()
+
+TABLE_NAME = 'customers'
 
 connection = pymysql.connect(
     host=os.environ['MYSQL_HOST'],
@@ -21,13 +24,24 @@ connection = pymysql.connect(
 cursor = connection.cursor()
 
 cursor.execute(
-    'CREATE TABLE IF NOT EXISTS customers (' 
+    f'CREATE TABLE IF NOT EXISTS {TABLE_NAME} (' 
     'id INT NOT NULL AUTO_INCREMENT, ' 
     'nome VARCHAR(50) NOT NULL, ' 
     'idade INT NOT NULL, ' 
     'PRIMARY KEY (id) '
     ') '
 )
+cursor.execute(f'TRUNCATE TABLE {TABLE_NAME}')
+
+cursor.execute(
+    f'INSERT INTO {TABLE_NAME} '
+    '(nome, idade) '
+    'VALUES '
+    '("rondi",35), '
+    '("samara", 28)'
+)
+
+connection.commit()
 
 cursor.close()
 connection.close()
