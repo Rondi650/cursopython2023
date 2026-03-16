c2a76a3109e009c6d8e520cf1bc85f557b998b6b
Rondi650 <rondinellenunes123@gmail.com>
2026-03-16 19:21:04 -0300

    criada tabela e funcionamento ok a conexao

--- aula206/main.py
+++ aula206/main.py
@@ -3,22 +3,31 @@
 # Pypy: https://pypi.org/project/pymysql/
 # GitHub: https://github.com/PyMySQL/PyMySQL
 import os
-from typing import cast
-
 import dotenv
 import pymysql
-import pymysql.cursors
 
 TABLE_NAME = 'customers'
-CURRENT_CURSOR = pymysql.cursors.DictCursor
 
+os.system('clear')
 dotenv.load_dotenv()
 
 connection = pymysql.connect(
     host=os.environ['MYSQL_HOST'],
     user=os.environ['MYSQL_USER'],
     password=os.environ['MYSQL_PASSWORD'],
-    database=os.environ['MYSQL_DATABASE'],
-    charset='utf8mb4',
-    cursorclass=CURRENT_CURSOR,
+    database=os.environ['MYSQL_DATABASE']
 )
+
+cursor = connection.cursor()
+
+cursor.execute(
+    'CREATE TABLE IF NOT EXISTS customers (' 
+    'id INT NOT NULL AUTO_INCREMENT, ' 
+    'nome VARCHAR(50) NOT NULL, ' 
+    'idade INT NOT NULL, ' 
+    'PRIMARY KEY (id) '
+    ') '
+)
+
+cursor.close()
+connection.close()
